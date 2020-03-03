3340380f608e3e92a266eac86a56c746d024ac99
Chip Covin <you@example.com>
2020-03-03 16:33:25 -0500

    Updates

---
+++ precache-manifest.68a7a5755bd352911dd66a665873a03c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76dd223d4626418f342c58197a1da529",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "57aa941b628f73c365d5",
+    "url": "/monsters-rolodex/static/css/main.09384695.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57aa941b628f73c365d5",
+    "url": "/monsters-rolodex/static/js/main.0aeb7e52.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
